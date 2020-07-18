fa6745967968f9975641e82e31c301563caf4cc4
Keshav <keshavthakur.001@gmail.com>
2020-07-18 18:01:33 +0530

    initialise heroku deployment

--- server/index.js
+++ server/index.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log("Listening");
 });
