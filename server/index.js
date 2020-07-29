e800803d5ec94ed54771b6206b305be62b864532
Keshav <keshavthakur.001@gmail.com>
2020-07-29 15:42:24 +0530

    Sovle minor bug in syntax in index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ mongoose
 app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build");
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile("../client/build/index.html");
