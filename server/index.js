bf9e3f7befd09d8216920cb8ae4e7885f613a120
Keshav <keshavthakur.001@gmail.com>
2020-07-29 16:19:36 +0530

    .

--- server/index.js
+++ server/index.js
@@ -30,7 +30,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/index.html"));
+    const index = path.join(__dirname, "../build", "index.html");
+    res.sendFile(index);
   });
 }
 
