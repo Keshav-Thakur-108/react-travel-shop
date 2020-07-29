cef06609543709758b493242f22af6fd73f6a412
Keshav <keshavthakur.001@gmail.com>
2020-07-29 15:05:28 +0530

    Minor change in index.js express.static

--- server/index.js
+++ server/index.js
@@ -25,10 +25,10 @@ mongoose
 
 app.use("/api", routes);
 
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
