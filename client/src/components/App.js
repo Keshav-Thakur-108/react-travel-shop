b6c4adfcd043822744f0c1cc92fbdfc7fa2dd0cd
Keshav <keshavthakur.001@gmail.com>
2020-07-30 23:30:52 +0530

    Add UploadProductPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Login from "../containers/Views/Login/Login";
 import Index from "../containers/Views/Index/index";
 import Register from "../containers/Views/Register/Register";
+import UploadProductPage from "../containers/Views/UploadProductPage/UploadProductPage";
 import * as actions from "../store/actions/auth";
 
 const App = (props) => {
@@ -13,9 +14,10 @@ const App = (props) => {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/" component={Index} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
-        <Route exact path="/" component={Index} />
+        <Route path="/product/upload" component={UploadProductPage} />
       </Switch>
     </div>
   );
---
+++ client/src/containers/Views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Typography, Button, Form, message, Input } from "antd";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const continents = [
+  { key: 1, value: "Africa" },
+  { key: 2, value: "Asia" },
+  { key: 3, value: "Australia" },
+  { key: 4, value: "Europe" },
+  { key: 5, value: "North America" },
+  { key: 6, value: "South America" },
+  { key: 7, value: "Antarctica" },
+];
+
+function UploadProductPage() {
+  const [titleValue, setTitleValue] = useState("");
+  const [descriptionValue, setDescriptionValue] = useState("");
+  const [priceValue, setPriceValue] = useState("");
+  const [continentValue, setContinentValue] = useState(1);
+
+  const onTitleChange = (event) => {
+    setTitleValue(event.target.value);
+  };
+  const onDescriptionChange = (event) => {
+    setDescriptionValue(event.target.value);
+  };
+  const onPriceChange = (event) => {
+    setPriceValue(event.target.value);
+  };
+
+  const onContinentChange = (event) => {
+    setContinentValue(event.target.value);
+  };
+
+  return (
+    <div
+      style={{
+        width: "700px",
+        margin: "2rem auto",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: "2rem",
+        }}
+      >
+        <Title>Upload Travel Product</Title>
+      </div>
+
+      <form>
+        <br />
+        <br />
+        <label>Title</label>
+        <Input
+          value={titleValue}
+          onChange={(e) => onTitleChange(e)}
+          type="text"
+        ></Input>
+        <br />
+        <br />
+
+        <label>Description</label>
+        <TextArea
+          value={descriptionValue}
+          onChange={(e) => onDescriptionChange(e)}
+        />
+        <br />
+        <br />
+
+        <label>Price($)</label>
+        <Input
+          value={priceValue}
+          onChange={(e) => onPriceChange(e)}
+          type="text"
+        ></Input>
+        <br />
+        <br />
+        <select onChange={(e) => onContinentChange(e)}>
+          {continents.map((continent) => (
+            <option key={continent.key} value={continent.key}>
+              {continent.value}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default UploadProductPage;
