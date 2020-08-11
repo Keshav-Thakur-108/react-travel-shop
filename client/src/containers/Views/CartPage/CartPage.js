d84e436e0b55cfdb15af65cc6add913c807154ed
Keshav <keshavthakur.001@gmail.com>
2020-08-11 17:15:09 +0530

    Add CartTemplate

--- client/src/containers/Views/CartPage/CartPage.js
+++ client/src/containers/Views/CartPage/CartPage.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../store/actions/user_actions";
+import UserCardBlock from "./Sections/UserCardBlock";
+import { Result, Empty } from "antd";
 
 function CartPage(props) {
+  const [Total, setTotal] = useState(0);
   const dispatch = useDispatch();
   useEffect(() => {
     let cartItems = [];
@@ -15,7 +18,54 @@ function CartPage(props) {
       }
     }
   }, [props.user.userData]);
-  return <div>CartPage</div>;
+
+  useEffect(() => {
+    if (props.user.cartDetail && props.user.cartDetail.length > 0)
+      calculateTotal(props.user.cartDetail);
+  }, [props.user.cartDetail]);
+
+  const calculateTotal = (cartDetail) => {
+    let total = 0;
+    cartDetail.map((item) => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+    setTotal(total);
+  };
+
+  return (
+    <div
+      style={{
+        width: "85%",
+        margin: "3rem auto",
+      }}
+    >
+      <h1>My Cart</h1>
+      <div>
+        <UserCardBlock products={props.user.cartDetail} />
+        <div
+          style={{
+            marginTop: "3rem",
+          }}
+        >
+          <h2>Total amount: ${Total}</h2>
+        </div>
+        <Result status="success" title="Successfully Purchased" />
+
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <br />
+          <Empty description={false} />
+          <p>No items in the cart</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartPage;
